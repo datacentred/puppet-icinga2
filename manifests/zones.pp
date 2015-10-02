e14cf3100eedef1a92a593c2a82b5be72644d19b
Simon Murray <simon.murray@datacentred.co.uk>
2015-10-02 14:17:52 +0100

    Add Dependencies For zones.conf
    Requires icinga2::install to create the directories and notifies
    icinga2::service of any modifications that will be picked up by a reload

--- manifests/zones.pp
+++ manifests/zones.pp
@@ -11,4 +11,9 @@ class icinga2::zones {
     mode   => '0644',
   }
 
+  # Ensure packages are installed first and notify the service of changes
+  Class['::icinga2::install'] ->
+  Class['::icinga2::zones'] ~>
+  Class['::icinga2::service']
+
 }
