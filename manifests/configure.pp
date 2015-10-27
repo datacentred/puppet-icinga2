0fc691bdf8ebbdf076cf16023517f9c90f83231c
Simon Murray <spjmurray@yahoo.co.uk>
2015-10-27 10:28:19 +0000

    Add TimePeriod Support
    Allows creation of timeperiods for downtime and notifications.  This change
    brings in management for conf.d therefore any stray files will be purged from
    the local configuration direcory

--- manifests/configure.pp
+++ manifests/configure.pp
@@ -5,14 +5,15 @@
 class icinga2::configure {
 
   file { [
+    '/etc/icinga2/conf.d',
     '/etc/icinga2/repository.d/endpoints',
     '/etc/icinga2/repository.d/zones',
     '/etc/icinga2/repository.d/hosts',
   ] :
     ensure  => directory,
-    owner   => 'nagios',
-    group   => 'nagios',
-    mode    => '0750',
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0755',
     recurse => true,
     purge   => true,
   }
---
+++ manifests/object/timeperiod.pp
@@ -0,0 +1,23 @@
+# == Define: icinga2::object::timeperiod
+#
+# Defines a time period for downtimes and notifications
+#
+define icinga2::object::timeperiod (
+  $import = undef,
+  $display_name = undef,
+  $ranges = undef,
+) {
+
+  $target = '/etc/icinga2/conf.d/timeperiods.conf'
+
+  if ! defined(Icinga2::Config[$target]) {
+    icinga2::config { $target: }
+  }
+
+  concat::fragment { "icinga2::object::timeperiod ${title}":
+    target  => $target,
+    content => template('icinga2/object/timeperiod.conf.erb'),
+    order   => '10',
+  }
+
+}
