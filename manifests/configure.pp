00e331fc866b86cff0b219911eca63e558696958
Simon Murray <simon.murray@datacentred.co.uk>
2015-11-18 11:03:43 +0000

    Allow Icinga2 Configuration
    A pervious change accidentally purged /etc/icinga2 when /etc/icinga2/zones.conf was
    defined.  This adds in support for management of init.conf, icinga2.conf and
    constants.conf to rectify the issue.  This only adds in the framework and does not
    allow actual configuration.

--- manifests/configure.pp
+++ manifests/configure.pp
@@ -4,16 +4,36 @@
 #
 class icinga2::configure {
 
+  File {
+    owner => 'root',
+    group => 'root',
+    mode  => '0644',
+  }
+
   file { [
+    '/etc/icinga2',
     '/etc/icinga2/conf.d',
     '/etc/icinga2/zones.d',
   ] :
     ensure  => directory,
-    owner   => 'root',
-    group   => 'root',
     mode    => '0755',
     recurse => true,
     purge   => true,
+  }
+
+  file { '/etc/icinga2/init.conf':
+    ensure  => file,
+    content => template('icinga2/init.conf.erb'),
+  }
+
+  file { '/etc/icinga2/icinga2.conf':
+    ensure  => file,
+    content => template('icinga2/icinga2.conf.erb'),
+  }
+
+  file { '/etc/icinga2/constants.conf':
+    ensure  => file,
+    content => template('icinga2/constants.conf.erb'),
   }
 
 }
