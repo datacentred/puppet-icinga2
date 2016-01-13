486d3f5a4b1f5479a8423b78aec6de39f108be94
Simon Murray <simon.murray@datacentred.co.uk>
2016-01-13 11:50:00 +0000

    Improve Template Generation
    Creates a utility function that can be used in templates to generate
    correct syntax.  It translates native booleans, integers and floats
    to unquoted parameters.  Regular strings are parsed to determine whether
    they are time periods, references to hosts, services, users or key/value
    pairs in 'apply service for' objects, or preformatted strings.  Again
    these are unquoted, anything else is assumed a string expression and
    quoted.  Hashes are also supported as is simple detection of whether
    keys need to be quoted as they contain illegal characters e.g. -b in
    check command arguments

---
+++ lib/puppet/parser/functions/icinga2_config_value.rb
@@ -0,0 +1,48 @@
+# Configuration variable generator
+#
+# Examines the input expression for intrinsic types (numeric, duration or boolean)
+# and omits quotes if specified in isolation.  The same applies for host or service
+# lookups, and optionally value lookups from within 'apply Service for' blocks.
+# inputs which feature double quoted values are assumed to already be correctly
+# formatted.  Anything else is assumed to be a string literal and is emitted with
+# double quotation.
+#
+def _icinga2_config_value(value, locals, depth=1)
+  # Hashes
+  if value.is_a?(Hash)
+    "{\n" + value.sort_by{|k,v|k}.inject('') do |a, x|
+      key = x.first
+      # If the key features illegal characters (e.g. '-d' in check command
+      # arguments) then quote the key
+      key = %Q/"#{key}"/ if key.include?('-')
+      "#{a}  #{'  ' * depth} #{key} = #{_icinga2_config_value(x.last, locals, depth + 1).to_s}\n"
+    end + ("  " * depth) + "}"
+  # Numeric literals
+  elsif value.is_a?(Integer) or value.is_a?(Float)
+    value
+  # Boolean literals
+  elsif value.is_a?(TrueClass) or value.is_a?(FalseClass)
+    value
+  # Duration literals
+  elsif /^\d+(\.\d+)?(ms|s|m|h|d)$/.match(value)
+    value
+  # Pre-formatted
+  elsif value.include?('"')
+    value
+  # Apply variables
+  elsif value.start_with?('host.', 'service.', 'user.')
+    value
+  # Apply for local variables
+  elsif value.start_with?(*locals.map{|x| "#{x}."})
+    value
+  # String literal
+  else
+    "\"#{value}\""
+  end
+end
+
+module Puppet::Parser::Functions
+  newfunction(:icinga2_config_value, :type => :rvalue) do |args|
+    _icinga2_config_value(args[0], args[1..-1])
+  end
+end
